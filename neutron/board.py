1760d9f0efae29171abb07e3281e37da6cf8a369
gvandestee <gvandesteeg@illumina.com>
2019-06-13 13:59:54 +0100

    Initial board state

--- neutron/board.py
+++ neutron/board.py
@@ -1,6 +1,22 @@
-class Tile:
-    def __init__(self):
+from pprint import pprint
+from string import ascii_uppercase
+
+
+class Tile(list):
+    def __init__(self, *values):
+        super().__init__(list(values) or [])
 
 
 class Board:
-    pass
+    def __init__(self, x: int, y: int):
+        self.board = [{i: {j: Tile() for j in range(y)} for i in ascii_uppercase[:x]}]
+
+
+    # def __repr__(self):
+    #     return '\n'.join([' '.join(map(str, row)) for row in self.board])
+
+
+b = Board(5, 5)
+
+pprint(b.board)
+
