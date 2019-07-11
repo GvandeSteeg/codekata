0dc9c6bbb9ee13a63251753c4ab46c24a4f89950
gvandestee <gvandesteeg@illumina.com>
2019-07-11 14:08:07 +0100

    reformat

--- wordlist_finder/__main__.py
+++ wordlist_finder/__main__.py
@@ -7,7 +7,6 @@ word_finder_list = ['C', 'R', 'C', 'E', 'I', 'N', 'E', 'C', 'T']
 permutation_words = set()
 for i in range(4, 10):
     permutation_words.update(permutations(word_finder_list, i))
-
 permutation_words = {''.join(x).lower() for x in permutation_words if 'I' in x}
 
 with open(os.path.abspath(
@@ -16,9 +15,5 @@ with open(os.path.abspath(
 
 result = {word for word in permutation_words if word in dictwords}
 
-print(len('eccentric'))
-print('eccentric' in dictwords)
 print(len(result))
-print(len(dictwords))
-
 pprint(result)
