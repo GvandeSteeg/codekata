a56df580a5179db6ebb443e662c45b0f280602bc
gvandestee <gvandesteeg@illumina.com>
2019-07-11 14:03:48 +0100

    Fix filtering bug

--- wordlist_finder/__main__.py
+++ wordlist_finder/__main__.py
@@ -12,10 +12,12 @@ permutation_words = {''.join(x).lower() for x in permutation_words if 'I' in x}
 
 with open(os.path.abspath(
         os.path.join(os.path.dirname(__file__), '..', 'resources', 'english-words', 'words_alpha.txt'))) as f:
-    dictwords = {l.strip() for l in f if len(l) >= 4 and len(l) <= 9}
+    dictwords = {l.strip() for l in f if 4 <= len(l.strip()) <= 9}
 
 result = {word for word in permutation_words if word in dictwords}
 
+print(len('eccentric'))
+print('eccentric' in dictwords)
 print(len(result))
 print(len(dictwords))
 
