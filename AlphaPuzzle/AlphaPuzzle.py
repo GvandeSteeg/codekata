c62fceb4c661d66ccc082a850317cfb2b531a628
gvandestee <gvandesteeg@illumina.com>
2019-07-25 15:18:55 +0100

    Added unique crossings

--- AlphaPuzzle/AlphaPuzzle.py
+++ AlphaPuzzle/AlphaPuzzle.py
@@ -2,10 +2,10 @@ import itertools
 import json
 import re
 from pprint import pprint
-from typing import List
+from typing import List, Union, Tuple, Dict, Set, Sequence
 
 
-def load_values():
+def load_values() -> Tuple[List[List[str]], Dict[str, List[List[str]]]]:
     with open("day1.json") as f:
         json_o = json.load(f)
         board = json_o["board"]
@@ -13,7 +13,7 @@ def load_values():
         return board, letters
 
 
-def get_all_words(board):
+def get_all_words(board: Sequence[Sequence[str]]) -> List[str]:
     words = []
     for row in board:
         word = " ".join(map(str, row)).split("0")
@@ -23,20 +23,18 @@ def get_all_words(board):
     return words
 
 
-def get_word_dict():
+def get_word_dict() -> Set[str]:
     with open("../resources/english-words/words_alpha.txt") as f:
         words_alpha_list = {l.strip() for l in f}
     return words_alpha_list
 
 
-board, letters = load_values()
-words = get_all_words(board)
+def replacer(wordlist: Sequence[Sequence[str]]):
+    """
+    For each known in the cypher, replace each integer in the wordlist with the actual letter
 
-board_t = list(zip(*board))
-words.extend(get_all_words(board_t))
-
-
-def replacer(wordlist: List[str]):
+    Words are lists of strings
+    """
     for word in wordlist:
         for i in range(len(word)):
             try:
@@ -46,15 +44,26 @@ def replacer(wordlist: List[str]):
     return wordlist
 
 
-print(len(words))
-print(words)
+board, letters = load_values()
+words = get_all_words(board)
+
+board_t = list(zip(*board))
+words.extend(get_all_words(board_t))
 
 words = replacer(words)
 unique_letters = set(itertools.chain(*words))
-x = dict()
+dict_of_things = dict()
 for i in unique_letters:
-    x[i] = [u for u in words if i in u]
+    dict_of_things[i] = [u for u in words if i in u]
 
-z = list(itertools.permutations(x['4'], 2))
+my_set = set()
+for x in dict_of_things.values():
+    x = set(map(tuple, x))
+    a = itertools.permutations(x, 2)
+    for i in a:
+        my_set.add(tuple(sorted(i)))
 
-print(z)
+pprint(sorted(my_set)[:50])
+print(len(my_set))
+
+# pprint(z)
